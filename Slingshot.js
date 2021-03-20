00d160d7532be07c554ae5b4c48f53eb78fd4bd9
sandeepjhaVC <heyyoumailmeat@gmail.com>
2021-03-20 17:56:56 +0530

    Slingshot

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot {
+    constructor(bodyA, pointb) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointb,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display() {
+        if (this.sling.bodyA) {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+}
